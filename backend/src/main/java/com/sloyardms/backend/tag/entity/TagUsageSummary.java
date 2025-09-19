d4c7211b22fc1442e8e6be24b767101bc9c20a34
sloyardms <sloyard_ms@outlook.com>
2025-09-18 19:06:18 -0700

    refactor(entity): rename columns in TagUsageSummary to correct names

--- backend/src/main/java/com/sloyardms/backend/tag/entity/TagUsageSummary.java
+++ backend/src/main/java/com/sloyardms/backend/tag/entity/TagUsageSummary.java
@@ -19,18 +19,18 @@ import java.util.UUID;
 public class TagUsageSummary {
 
     @Id
-    @Column(name = "user_uuid", nullable = false)
+    @Column(name = "user_id", nullable = false)
     private UUID userId;
 
     @Id
-    @Column(name = "group_uuid", nullable = false)
+    @Column(name = "group_id", nullable = false)
     private UUID groupId;
 
     @Id
-    @Column(name = "tag_uuid", nullable = false)
+    @Column(name = "tag_id", nullable = false)
     private UUID tagId;
 
-    @Column(name = "bookmark_count", nullable = false)
+    @Column(name = "item_count", nullable = false)
     @Min(0)
     private int itemCount;
 
