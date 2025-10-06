bc28b12ce8bc2446385a333c4799be00bc0a187c
sloyardms <sloyard_ms@outlook.com>
2025-10-06 00:00:54 -0700

    refactor(security): cleanup DevSecurityConfig

--- backend/src/main/java/com/sloyardms/backend/security/config/DevSecurityConfig.java
+++ backend/src/main/java/com/sloyardms/backend/security/config/DevSecurityConfig.java
@@ -1,12 +1,14 @@
-package com.sloyardms.backend.security;
+package com.sloyardms.backend.security.config;
 
+import com.sloyardms.backend.security.filter.FakeAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Profile({"dev", "test"})
@@ -15,9 +17,9 @@ public class DevSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(new FakeAuthFilter(), AbstractPreAuthenticatedProcessingFilter.class);
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .addFilterBefore(new FakeAuthFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
