b6f94d1fb8d125ff910879ff62ccfb67e449debf
sloyardms <sloyard_ms@outlook.com>
2025-10-06 00:00:22 -0700

    refactor(user): add username property and initialize filter list in User entity

--- backend/src/main/java/com/sloyardms/backend/user/entity/User.java
+++ backend/src/main/java/com/sloyardms/backend/user/entity/User.java
@@ -10,16 +10,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User extends Auditable {
+
+    public User() {
+        this.settings = UserSettings.withDefaults(null);
+    }
 
     @Id
     @Column(name = "id", nullable = false, updatable = false)
@@ -28,17 +32,20 @@ public class User extends Auditable {
     @Column(name = "external_id", unique = true, nullable = false)
     private UUID externalId;
 
+    @Column(name = "username", unique = true, nullable = false)
+    private String userName;
+
     @Type(JsonType.class)
     @Column(name = "settings", columnDefinition = "jsonb", nullable = false)
     private UserSettings settings;
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private List<UserFilter> filters = new ArrayList<>();
 
     @PostLoad
     private void ensureDefaultSettings(){
         this.settings = UserSettings.withDefaults(this.settings);
     }
-
-    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY, orphanRemoval = true)
-    @JoinColumn(name = "user_id")
-    private List<UserFilter> filters;
 
 }
