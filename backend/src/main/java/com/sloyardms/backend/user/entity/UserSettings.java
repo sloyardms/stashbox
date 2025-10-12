a4e3f58f2fcfb4753c4ea11029b572725689413c
sloyardms <sloyard_ms@outlook.com>
2025-10-12 09:25:36 -0700

    feat(user): add builder annotation to UserSettings

--- backend/src/main/java/com/sloyardms/backend/user/entity/UserSettings.java
+++ backend/src/main/java/com/sloyardms/backend/user/entity/UserSettings.java
@@ -1,12 +1,14 @@
 package com.sloyardms.backend.user.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserSettings {
