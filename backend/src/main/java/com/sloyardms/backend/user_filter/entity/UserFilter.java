be69a13689329ad92352f79142bf0ef6fa87e6af
sloyardms <sloyard_ms@outlook.com>
2025-10-12 09:25:15 -0700

    feat(user): refactor filters relatioship in user entity

--- backend/src/main/java/com/sloyardms/backend/user_filter/entity/UserFilter.java
+++ backend/src/main/java/com/sloyardms/backend/user_filter/entity/UserFilter.java
@@ -1,8 +1,17 @@
 package com.sloyardms.backend.user_filter.entity;
 
 import com.sloyardms.backend.common.entity.Auditable;
-import jakarta.persistence.*;
+import com.sloyardms.backend.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,12 +20,13 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(
         name = "user_filters", uniqueConstraints = {
-                @UniqueConstraint(name = "user_filter_url_pattern_unique", columnNames = {"user_id", "url_pattern"})
+        @UniqueConstraint(name = "user_filter_url_pattern_unique", columnNames = {"user_id", "url_pattern"})
 })
 public class UserFilter extends Auditable {
 
@@ -24,8 +34,9 @@ public class UserFilter extends Auditable {
     @Column(nullable = false, updatable = false)
     private UUID id;
 
-    @Column(name = "user_id", nullable = false)
-    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(name = "filter_name", nullable = false)
     private String filterName;
