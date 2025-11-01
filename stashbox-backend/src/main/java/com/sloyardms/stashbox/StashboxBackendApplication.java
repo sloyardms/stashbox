4c457df6c3b70ff23b3b4873913f67aaab176e5f
sloyardms <sloyard_ms@outlook.com>
2025-10-31 19:35:37 -0700

    refactor(main): remove redundant `public` modifier from main method

--- stashbox-backend/src/main/java/com/sloyardms/stashbox/StashboxBackendApplication.java
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/StashboxBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class StashboxBackendApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(StashboxBackendApplication.class, args);
     }
 
