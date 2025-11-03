eb1d44910eeb22d6515437898572ea9cda2a69a4
sloyardms <sloyard_ms@outlook.com>
2025-11-02 19:14:56 -0700

    feat(User): add cacheable method to get user ID by external Keycloak ID

--- stashbox-backend/src/main/java/com/sloyardms/stashbox/user/UserRepository.java
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/user/UserRepository.java
@@ -1,12 +1,19 @@
 package com.sloyardms.stashbox.user;
 
 import com.sloyardms.stashbox.user.entity.User;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Cacheable(value = "userIdByExternalId", key = "#externalId")
+    @Query("SELECT u.id FROM User u WHERE u.externalId = :externalId")
+    Optional<UUID> findIdByExternalId(@Param("externalId") UUID externalId);
 
     Optional<User> findByExternalId(UUID externalId);
 
