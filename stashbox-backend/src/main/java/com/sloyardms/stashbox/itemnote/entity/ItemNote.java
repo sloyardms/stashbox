a3e382399c1b905a80a6cde59a032c44cf0ebffa
sloyardms <sloyard_ms@outlook.com>
2025-11-04 00:06:19 -0700

    feat(ItemNote): add ItemNote entity

---
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/itemnote/entity/ItemNote.java
@@ -0,0 +1,51 @@
+package com.sloyardms.stashbox.itemnote.entity;
+
+import com.sloyardms.stashbox.common.entity.Auditable;
+import com.sloyardms.stashbox.stashitem.entity.StashItem;
+import com.sloyardms.stashbox.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+@Table(name = "item_notes", indexes = {
+        @Index(name = "item_notes_item_user_created_index", columnList = "item_id, user_id, created_at")
+})
+public class ItemNote extends Auditable {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "item_id", nullable = false)
+    private StashItem item;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "note", columnDefinition = "TEXT", length = 500)
+    private String note;
+
+}
