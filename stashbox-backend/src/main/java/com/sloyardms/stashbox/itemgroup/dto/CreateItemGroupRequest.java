d59092a3ec06dcdebb2b0edda8df5d814fa7dae6
sloyardms <sloyard_ms@outlook.com>
2025-11-06 15:39:57 -0700

    feat(ItemGroup): add CreateItemGroupRequest DTO

---
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/itemgroup/dto/CreateItemGroupRequest.java
@@ -0,0 +1,26 @@
+package com.sloyardms.stashbox.itemgroup.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class CreateItemGroupRequest {
+
+    @NotBlank(message = "{itemGroup.name.notBlank}")
+    @Size(max = 50, message = "{itemGroup.name.maxSize}")
+    private String name;
+
+    @Size(max = 255, message = "{itemGroup.description.maxSize}")
+    private String description;
+
+}
