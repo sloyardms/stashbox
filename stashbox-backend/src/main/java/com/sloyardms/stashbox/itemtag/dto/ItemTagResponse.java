4b8f9adea44fb314949a5746e308a04fd4cf1670
sloyardms <sloyard_ms@outlook.com>
2025-11-06 02:45:57 -0700

    feat(ItemTag): add ItemTagResponse DTO

---
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/itemtag/dto/ItemTagResponse.java
@@ -0,0 +1,26 @@
+package com.sloyardms.stashbox.itemtag.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ItemTagResponse {
+
+    private UUID id;
+    private String name;
+    private String slug;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+}
