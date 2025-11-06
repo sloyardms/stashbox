31de1f684f09db91f5ee38dbc11d9723518459f5
sloyardms <sloyard_ms@outlook.com>
2025-11-05 18:37:13 -0700

    feat(User): add mapper for User entity

---
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/user/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.sloyardms.stashbox.user.mapper;
+
+import com.sloyardms.stashbox.user.dto.UserResponse;
+import com.sloyardms.stashbox.user.dto.UserSummaryResponse;
+import com.sloyardms.stashbox.user.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {UserSettingsMapper.class})
+public interface UserMapper {
+
+    UserResponse toResponse(User user);
+
+    UserSummaryResponse toSummaryResponse(User user);
+
+}
