db01d1b8ee586b29c740a0b19316b5cab5fca133
sloyardms <sloyard_ms@outlook.com>
2025-11-12 23:55:59 -0700

    fix(UserRepository): remove unused code

--- stashbox-backend/src/main/java/com/sloyardms/stashbox/user/repository/UserRepository.java
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/user/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package com.sloyardms.stashbox.user.repository;
 
 import com.sloyardms.stashbox.user.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -12,7 +10,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByExternalId(UUID externalId);
-
-    Page<User> findByExternalIdNot(UUID externalId, Pageable pageable);
 
 }
