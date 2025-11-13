b796d7430a49ee144c22b56b1d71d067260324cc
sloyardms <sloyard_ms@outlook.com>
2025-11-12 23:44:20 -0700

    feat(User): add UserService interface and implementation for basic CRUD

---
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/user/service/UserServiceImpl.java
@@ -0,0 +1,117 @@
+package com.sloyardms.stashbox.user.service;
+
+import com.sloyardms.stashbox.common.error.exception.ResourceAlreadyExistsException;
+import com.sloyardms.stashbox.common.error.exception.ResourceNotFoundException;
+import com.sloyardms.stashbox.common.utils.PageableValidator;
+import com.sloyardms.stashbox.user.dto.AdminUserResponse;
+import com.sloyardms.stashbox.user.dto.UpdateUserSettingsRequest;
+import com.sloyardms.stashbox.user.dto.UserResponse;
+import com.sloyardms.stashbox.user.entity.User;
+import com.sloyardms.stashbox.user.entity.UserSettings;
+import com.sloyardms.stashbox.user.mapper.UserMapper;
+import com.sloyardms.stashbox.user.mapper.UserSettingsMapper;
+import com.sloyardms.stashbox.user.repository.UserRepository;
+import com.sloyardms.stashbox.user.specification.UserSpecification;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    private static final Set<String> ALLOWED_SORT_FIELDS = Set.of("username", "email", "createdAt", "updatedAt");
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final UserSettingsMapper userSettingsMapper;
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserResponse getUserByExternalId(UUID userExternalId) {
+        User user = userRepository.findByExternalId(userExternalId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userExternalId));
+        return userMapper.toResponse(user);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Page<AdminUserResponse> getAllUsersExcludingCurrent(UUID currentAdminExternalId, String searchQuery,
+                                                               Pageable pageable) {
+
+        PageableValidator.validateSortFields(pageable, ALLOWED_SORT_FIELDS);
+
+        Specification<User> spec = Specification.allOf(
+                UserSpecification.excludingUser(currentAdminExternalId),
+                UserSpecification.search(searchQuery)
+        );
+
+        Page<User> users = userRepository.findAll(spec, pageable);
+        return users.map(userMapper::toAdminResponse);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public UserResponse saveUser(UUID externalId, String username, String email) {
+        User user = new User();
+        user.setId(UUID.randomUUID());
+        user.setExternalId(externalId);
+        user.setUsername(username);
+        user.setEmail(email);
+
+        user = saveUserChanges(user);
+        return userMapper.toResponse(user);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void deleteUserByInternalId(UUID internalId) {
+        User user = userRepository.findById(internalId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", internalId));
+        userRepository.delete(user);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void deleteUserByExternalId(UUID userExternalId) {
+        User user = userRepository.findByExternalId(userExternalId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "externalId", userExternalId));
+        userRepository.delete(user);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public UserResponse updateUserSettings(UpdateUserSettingsRequest updateUserSettingsRequest, UUID userExternalId) {
+        User user = userRepository.findByExternalId(userExternalId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "externalId", userExternalId));
+        UserSettings newSettings = userSettingsMapper.updateFromDto(updateUserSettingsRequest, user.getSettings());
+        user.setSettings(newSettings);
+        user = saveUserChanges(user);
+        return userMapper.toResponse(user);
+    }
+
+    private User saveUserChanges(User user) {
+        try {
+            return userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            String message = e.getMessage();
+            if (message != null && message.contains("users_external_id_unique")) {
+                throw new ResourceAlreadyExistsException("User", "External Id", user.getExternalId().toString());
+            }
+            if (message != null && message.contains("users_username_unique")) {
+                throw new ResourceAlreadyExistsException("User", "Username", user.getUsername());
+            }
+            if (message != null && message.contains("users_username_email")) {
+                throw new ResourceAlreadyExistsException("User", "Email", user.getEmail());
+            }
+            throw e;
+        }
+    }
+
+}
