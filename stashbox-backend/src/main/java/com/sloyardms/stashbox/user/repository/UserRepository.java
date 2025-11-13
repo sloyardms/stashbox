451d19792fc5532febf3faf06fa93584a175b4c0
sloyardms <sloyard_ms@outlook.com>
2025-11-12 23:42:24 -0700

    feat(User): update UserRepository with new methods

--- stashbox-backend/src/main/java/com/sloyardms/stashbox/user/repository/UserRepository.java
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/user/repository/UserRepository.java
@@ -1,22 +1,18 @@
 package com.sloyardms.stashbox.user.repository;
 
 import com.sloyardms.stashbox.user.entity.User;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-
-    @Cacheable(value = "userIdByExternalId", key = "#externalId")
-    @Query("SELECT u.id FROM User u WHERE u.externalId = :externalId")
-    Optional<UUID> findIdByExternalId(@Param("externalId") UUID externalId);
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByExternalId(UUID externalId);
 
-    Long deleteByExternalId(UUID externalId);
+    Page<User> findByExternalIdNot(UUID externalId, Pageable pageable);
 
 }
