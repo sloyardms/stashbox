8d0b5f3ef13a6c8db1ed43954c024b4e7df35cd0
sloyardms <sloyard_ms@outlook.com>
2025-12-06 16:48:25 -0700

    refactor(messages): remove WebMvcConfigurer implementation in MessageSourceConfiguration

--- stashbox-backend/src/main/java/com/sloyardms/stashbox/config/messages/MessageSourceConfiguration.java
+++ stashbox-backend/src/main/java/com/sloyardms/stashbox/config/messages/MessageSourceConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MessageSourceConfiguration {
+public class MessageSourceConfiguration implements WebMvcConfigurer {
 
     @Bean
     @Primary
@@ -31,6 +33,11 @@ public class MessageSourceConfiguration {
         LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
         validator.setValidationMessageSource(messageSource);
         return validator;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator(messageSource());
     }
 
 }
